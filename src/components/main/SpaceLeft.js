e1e353d97c0066388149a03d4fdcff9ba58a79d4
TrinhDuc997 <trinhxuanduc997@gmail.com>
2023-02-24 17:58:10 +0700

    fix scroll

--- src/components/main/SpaceLeft.js
+++ src/components/main/SpaceLeft.js
@@ -1,13 +1,30 @@
-import { Box } from "@mui/system";
+import { Box } from "@mui/material";
 import React from "react";
 import CalendarTree from "./CalendarTree";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import { IconButton } from "@mui/material";
+import { scrollbarBackgroundColorLightMode } from "../../utils/theme";
 
 function SpaceLeft(props) {
   const { handleChangeDate, handleDrawerClose } = props;
   return (
-    <Box width={"100%"} height={"100%"} overflow={"auto"}>
+    <Box
+      width={"100%"}
+      height={"100%"}
+      overflow={"auto"}
+      sx={{
+        "&::-webkit-scrollbar": {
+          backgroundColor: "background.default",
+          position: "absolute",
+          borderRadius: "10px",
+          width: "10px",
+        },
+        "&::-webkit-scrollbar-thumb": {
+          backgroundColor: "background.scrollColor",
+          borderRadius: "10px",
+        },
+      }}
+    >
       <IconButton
         onClick={handleDrawerClose}
         sx={{ position: "absolute", right: "0px", zIndex: 999 }}
