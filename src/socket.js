d70cd6ad3d02f848a42d451cb32feb1b9b0767c2
TrinhDuc997 <trinhxuanduc997@gmail.com>
2023-03-23 14:54:44 +0700

    fix socket IO

--- src/socket.js
+++ src/socket.js
@@ -3,4 +3,7 @@ import { io } from "socket.io-client";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.REACT_APP_API_URL;
 
-export const socket = io(URL, { reconnectionAttempts: 5 });
+export const socket = io(URL, {
+  reconnectionAttempts: 5,
+  //   reconnection: false,
+});
